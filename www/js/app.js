ec3322c5aa4a029b0453239d547d40d56a7ef387
usman dabay khan <usman10scorpio@gmail.com>
2017-08-04 17:02:18 +0500

    ionic map in partial

--- www/js/app.js
+++ www/js/app.js
@@ -3,25 +3,39 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
+app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-app.controller('main', function($scope) { //store the entities name in a variable var taskData = 'task';
-        $scope.hello = "hello world";
-    })
-    .run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-                // Don't remove this line unless you know what you are doing. It stops the viewport
-                // from snapping when text inputs are focused. Ionic handles this internally for
-                // a much nicer keyboard experience.
-                cordova.plugins.Keyboard.disableScroll(true);
-            }
-            if (window.StatusBar) {
-                StatusBar.styleDefault();
-            }
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('index', {
+            url: '/index',
+            templateUrl: 'templates/main.html',
+            controller: 'main'
+        })
+        .state('schools', {
+            url: "/schools",
+            templateUrl: "templates/schools.html",
+            controller: "schools"
+        }).state('banks', {
+            url: "/banks",
+            templateUrl: "templates/banks.html",
+            controller: "banks"
         });
-    })
+    $urlRouterProvider.otherwise('/index');
+});
